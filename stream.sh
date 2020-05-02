531e6e8166509b3128fdc3342a622750f1dcda08
Daniel <djwf75@gmail.com>
2020-05-02 17:44:03 +0100

    added bash file

---
+++ stream.sh
@@ -0,0 +1,1 @@
+raspivid -t 0 -w 1280 -h 720 -fps 25 -g 75 -fl -o | ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -i pipe:0 -c:v copy -c:a aac -strict experimental -f flv -f flv rtmp://192.168.0.44:3000/app/stream
