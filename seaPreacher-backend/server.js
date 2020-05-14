37dbf35072dbdc48904a9009aac34dacf1679b67
Daniel <djwf75@gmail.com>
2020-05-14 19:46:17 +0100

    video-stream

--- seaPreacher-backend/server.js
+++ seaPreacher-backend/server.js
@@ -7,6 +7,7 @@ const port = 8000;
 // * for testing only *
 app.use(cors());
 app.use(express.json());
+const raspividStream = require("raspivid-stream");
 
 const expressServer = app.listen(port, () => {
   console.log("app running on", port);
@@ -61,3 +62,32 @@ async function wifiData() {
 
 batteryData();
 wifiData();
+
+const wss = require("express-ws")(app);
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+app.ws("/video-stream", (ws, req) => {
+  console.log("Client connected");
+  ws.send(
+    JSON.stringify({
+      action: "init",
+      width: "960",
+      height: "540",
+    })
+  );
+
+  var videoStream = raspividStream({ rotation: 180 });
+
+  videoStream.on("data", (data) => {
+    ws.send(data, { binary: true }, (error) => {
+      if (error) console.error(error);
+    });
+  });
+
+  ws.on("close", () => {
+    console.log("Client left");
+
+    videoStream.removeAllListeners("data");
+  });
+});
